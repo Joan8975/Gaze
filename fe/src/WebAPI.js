41c2e4688424490c8aab89586abd2b7724a12321
Joan Lin <joan.lin@octon.net>
2021-01-08 12:11:42 +0800

    fix: update new api(express)

--- fe/src/WebAPI.js
+++ fe/src/WebAPI.js
@@ -17,35 +17,45 @@ fetch(`https://words.bighugelabs.com/api/2/55fd0122831fdaadd4c00e498f7a1b73/${wo
 
 export const singleImg = (imgId) => fetch(`https://api.unsplash.com/photos/${imgId}?client_id=773741e75ba8c52b7d3d825cd4c33cf637a1f77a7fe0f64109e4f5bdd35e22ad`).then(res => res.json())
 
-export const allCollections = (email) => fetch(`https://joandes.com/Gaze/api/allCollections.php?email=${email}`).then(res => res.json())
+export const allCollections = (email) =>
+  fetch(`https://gaze.joandes.com/collections/${email}`).then((res) =>
+    res.json()
+  );
 
-export const postImg = (newSave) => fetch('https://joandes.com/Gaze/api/saves.php',{
-  method: 'POST',
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(newSave),
-  }).then(res => res.json())
+export const postImg = (newSave) =>
+  fetch("https://gaze.joandes.com/createImage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newSave),
+  }).then((res) => res.json());
 
-export const allSaves = (email) => fetch(`https://joandes.com/Gaze/api/saves.php?email=${email}`).then(res => res.json())
+export const allSaves = (email) =>
+  fetch(`https://gaze.joandes.com/images/${email}`).then((res) => res.json());
 
-export const deleteSave = (deleteSave) => fetch('https://joandes.com/Gaze/api/saves.php',{
-  method: 'DELETE',
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(deleteSave),
-  }).then(res => res.json())
+export const deleteSave = (deleteSave) =>
+  fetch("https://gaze.joandes.com/deleteImage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(deleteSave),
+  }).then((res) => res.json());
 
-export const collectionSaves = (email,collection) => fetch(`https://joandes.com/Gaze/api/collection.php?email=${email}&collection=${collection}`).then(res => res.json())
+export const collectionSaves = (email, collection) =>
+  fetch(
+    `https://gaze.joandes.com/images/${email}/${collection}`
+  ).then((res) => res.json());
 
-export const deleteCollection = (deleteCollection) => fetch('https://joandes.com/Gaze/api/collection.php',{
-  method: 'DELETE',
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(deleteCollection),
-  }).then(res => res.json())
+export const deleteCollection = (deleteCollection) =>
+  fetch("https://gaze.joandes.com/deleteCollection", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(deleteCollection),
+  }).then((res) => res.json());
 
 
 
